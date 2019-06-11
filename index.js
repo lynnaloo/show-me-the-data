8be3bd1128e173021dbb0c7325a3e3aa9f04b48b
Linda Nichols <lynnaloo@gmail.com>
2019-06-10 20:03:55 -0400

    [init] initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const PORT = process.env.PORT || 8000;
+
+const init = async () => {
+
+    const server = Hapi.server({
+      port: PORT,
+      host: 'localhost'
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => {
+        return 'Hello World!';
+      }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
