0297ed6362ad6334f212696db148f2d7b7c958e0
Linda Nichols <lynnaloo@gmail.com>
2019-08-20 18:06:20 -0400

    [update] add eslint config

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-'use strict';
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-process-env */
 
 const Hapi = require('@hapi/hapi');
+
 const PORT = process.env.PORT || 8000;
 
 const init = async () => {
 
-    const server = Hapi.server({
-      port: PORT,
-      host: 'localhost'
-    });
+  const server = Hapi.server({
+    port: PORT,
+    host: 'localhost',
+  });
 
-    server.route([
-      {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-          return 'Welcome to the Pet Store';
-        }
-      }, {
-        method: 'GET',
-        path: '/pet',
-        handler: (request, h) => {
-          return {
-            'id': '123456789',
-            'name': 'Spot',
-            'photoUrl': 'www.cute.com',
-            'status': 'available'
-          };
-        }
-      }
-    ]);
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: (request, handler) => {
+        return 'Welcome to the Pet Store';
+      },
+    }, {
+      method: 'GET',
+      path: '/pet',
+      handler: (request, handler) => {
+        return {
+          id: '123456789',
+          name: 'Spot',
+          photoUrl: 'www.cute.com',
+          status: 'available',
+        };
+      },
+    },
+  ]);
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
